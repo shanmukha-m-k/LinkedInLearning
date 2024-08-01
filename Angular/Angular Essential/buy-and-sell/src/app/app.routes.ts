76c55545ec1f0f38fd93a810f1971e7163756863
Shanmukha M K <56854991+shanmukha-m-k@users.noreply.github.com>
2024-08-01 21:14:35 +0530

    buy and sell initial setup
    buy and sell initial project setup

---
+++ Angular/Angular Essential/buy-and-sell/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+
+export const routes: Routes = [
+    {path:'listings',component:ListingsPageComponent,pathMatch:"full"}
+];
