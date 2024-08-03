c5a5e436328f6405d134e6f14b4dc994694fe196
Shanmukha M K <56854991+shanmukha-m-k@users.noreply.github.com>
2024-08-03 12:56:13 +0530

    backend project setup
    backend project setup

---
+++ Angular/Angular Essential/buy-and-sell/Backend/src/main/java/buyandsell/com/BuyAndSellApplication.java
@@ -0,0 +1,14 @@
+package buyandsell.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class BuyAndSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyAndSellApplication.class, args);
+	}
+
+}
