9d68cef52bd57036a4db72c4b1c8b13eacd07e4d
Shanmukha M K <56854991+shanmukha-m-k@users.noreply.github.com>
2024-08-03 12:49:23 +0530

    navbar updates
    navbar updated

--- Angular/Angular Essential/buy-and-sell/src/app/app.component.ts
+++ Angular/Angular Essential/buy-and-sell/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListingsPageComponent } from "./listings-page/listings-page.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListingsPageComponent],
+  imports: [RouterOutlet, ListingsPageComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
